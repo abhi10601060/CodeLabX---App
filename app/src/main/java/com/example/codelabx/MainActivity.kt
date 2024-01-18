d130e259cc1c04a548d636cb9b56b9591388466c
Abhi <abhishekvelekar79200@gmail.com>
2024-01-19 00:04:58 +0530

    languages spinner added

--- app/src/main/java/com/example/codelabx/MainActivity.kt
+++ app/src/main/java/com/example/codelabx/MainActivity.kt
@@ -2,13 +2,32 @@ package com.example.codelabx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.SpinnerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var spinner : Spinner
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        createView()
 
+    }
 
+    private fun createView() {
+        spinner = findViewById(R.id.languages_spinner)
+
+        setSpinner()
+    }
+
+    private fun setSpinner() {
+        val adapter = ArrayAdapter.createFromResource(this, R.array.languages , android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
