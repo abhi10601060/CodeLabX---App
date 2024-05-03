855a0512d30cd68b13c3888114b0d042914ec505
Abhi <abhishekvelekar79200@gmail.com>
2024-05-03 21:06:06 +0530

    auth screens added

--- app/src/main/java/com/example/codelabx/ui/fragments/LoginScreen.kt
+++ app/src/main/java/com/example/codelabx/ui/fragments/LoginScreen.kt
@@ -1,13 +1,33 @@
 package com.example.codelabx.ui.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.codelabx.R
 
 class LoginScreen : Fragment(R.layout.login_screen_layout) {
 
+    private lateinit var loginBtn : Button
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        createView(view)
+        setOnClickListeners()
+    }
+
+    private fun setOnClickListeners() {
+        loginBtn.setOnClickListener(OnClickListener {
+            findNavController().navigate(R.id.action_loginScreen_to_signuoScreen)
+        })
+    }
+
+    private fun createView(view : View) {
+        loginBtn = view.findViewById(R.id.login_btn)
     }
 }
--- app/src/main/java/com/example/codelabx/ui/fragments/StartFlashScreen.kt
+++ app/src/main/java/com/example/codelabx/ui/fragments/StartFlashScreen.kt
@@ -1,10 +1,12 @@
 package com.example.codelabx.ui.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.codelabx.R
 
 class StartFlashScreen : Fragment() {
@@ -15,6 +17,9 @@ class StartFlashScreen : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.start_flash_screen_layout , container , false)
 
+        Handler().postDelayed(Runnable {
+            findNavController().navigate(R.id.action_startFlashScreen_to_loginScreen)
+        } , 1000)
 
         return view
     }
